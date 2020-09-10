a7e6372c6f33223c38d1be89749ff3e06c96cf88
Bonnet <Bonnet@DESKTOP-LK7DJNB>
2020-09-10 18:52:01 +0200

    Ajout de script pour rendre le formulaire fonctionnel pour qu'il envoie en base de donnée.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,17 +5,35 @@ namespace App\Controller;
 use App\Entity\Materiel;
 use App\Form\MaterielType;
 use App\Repository\MaterielRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 
 class HomeController extends AbstractController
 {
-    public function index():Response
+    public function __construct(MaterielRepository $repository,EntityManagerInterface $em)
     {
-       $form = $this->createForm(MaterielType::class);
+        $this->repositery = $repository;
+        $this->em = $em;
+    }
+    public function index(Request $request):Response
+    {
+        $materiels = new Materiel();
+        $form = $this->createForm(MaterielType::class,$materiels);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+           $this->em->persist($materiels);
+            $this->em->flush();
+
+        }
         return $this->render('pages/home.html.twig',
             [
+                'materiel' => $materiels,
                 'form' => $form->createView()
             ]);
     }
