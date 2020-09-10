90b002512ca875d91bdd1eb7e7cfead019bc0d3f
Bonnet <Bonnet@DESKTOP-LK7DJNB>
2020-09-10 18:31:04 +0200

    Ajout du Formulaire dans la page Home à l'aide de php /bin/console make:form.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,21 @@
 namespace App\Controller;
 
 
+use App\Entity\Materiel;
+use App\Form\MaterielType;
+use App\Repository\MaterielRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 
 class HomeController extends AbstractController
 {
-    public function index () : Response
+    public function index():Response
     {
-        return $this->render('pages/home.html.twig');
+       $form = $this->createForm(MaterielType::class);
+        return $this->render('pages/home.html.twig',
+            [
+                'form' => $form->createView()
+            ]);
     }
 }
